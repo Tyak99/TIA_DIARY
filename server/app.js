e83fdfefa00e2af64d8873545ee649682ab02eec
Tunde Yakub <tundenasri@gmail.com>
2019-05-13 17:27:06 +0100

    set up entry app file

--- server/app.js
+++ server/app.js
@@ -2,5 +2,13 @@ import express from 'express';
 
 require('dotenv').config();
 
+const app = express();
 
-console.log(process.env.my_secret, '- server started');
+app.get('/', (req, res) => {
+  res.send('Welcome to diary app');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('server has started');
+});

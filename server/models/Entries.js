f57a977bf1a1f82e1df1bf675f635e14da908998
Tunde Yakub <tundenasri@gmail.com>
2019-05-15 18:44:21 +0100

    convert model files to es6 syntax

--- server/models/Entries.js
+++ server/models/Entries.js
@@ -1,11 +1,10 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Entries = sequelize.define('Entries', {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    userId: DataTypes.INTEGER
+    userId: DataTypes.INTEGER,
   }, {});
-  Entries.associate = function(models) {
+  Entries.associate = (models) => {
     Entries.belongsTo(models.User);
   };
   return Entries;
--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,11 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    timestamps: false
+    password: DataTypes.STRING,
   });
-  User.associate = function(models) {
+  User.associate = (models) => {
     User.hasMany(models.Entries);
   };
   return User;

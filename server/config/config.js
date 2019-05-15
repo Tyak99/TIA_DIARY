cb03e55f327c77e5e1bc2dda8bfe7ad7a8b89323
Tunde Yakub <tundenasri@gmail.com>
2019-05-15 16:51:08 +0100

    changed sequelize set up files to es6 syntax

--- server/config/config.js
+++ server/config/config.js
@@ -8,20 +8,20 @@ module.exports = {
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
-    dialect: 'mysql'
-  }
+    dialect: 'mysql',
+  },
 };

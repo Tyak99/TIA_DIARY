b5df930672f0a94bfbd2384e78906a5beecc77fa
Tunde Yakub <tundenasri@gmail.com>
2019-05-16 17:46:40 +0100

    remove timestamp from users table

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    },
+  );
   User.associate = (models) => {
     User.hasMany(models.Entries);
   };

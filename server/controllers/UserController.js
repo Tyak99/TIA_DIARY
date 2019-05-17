355e96992ec36c6d1d5695935d00ee11baa1f857
beejay1293 <mobolajimatti@Mobolajis-MacBook-Pro.local>
2019-05-17 12:47:50 +0100

    ft(user-login-endpoint): user login endpoint
    this feature enables users to login with thier valid credentials
    
    [Delievers 165954603]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,6 +2,34 @@ import models from '../models';
 
 const { User } = models;
 
-class UserController {}
+class UserController {
+
+    static login(req, res){
+        const { email, password } = req.body;
+        
+        User.findOne({ where: {email}})
+        .then(user => {
+            if(!user){
+                return res.status(401).json({
+                    status: 401,
+                    error: 'invalid credentials'
+                })   
+            }
+
+            if(user.password !== password){
+                return res.status(401).json({
+                    status: 401,
+                    error: 'invalid credentials'
+                })
+            }
+
+            return res.status(200).json({
+                status: 200,
+                data: user
+            })
+        })
+
+    }
+}
 
 export default UserController;
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,4 +3,10 @@ import userController from '../controllers/UserController';
 
 const router = express.Router();
 
+const { login } = userController;
+
+
+
+router.post('/login', login)
+
 export default router;

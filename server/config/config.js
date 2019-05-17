d859362704f7810cd3b6984300d99ff59ae3b882
Tunde Yakub <tundenasri@gmail.com>
2019-05-17 10:52:26 +0100

    add test configuration to sequelize config file

--- server/config/config.js
+++ server/config/config.js
@@ -11,11 +11,11 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    dialect: 'postgres',
   },
   production: {
     username: 'root',

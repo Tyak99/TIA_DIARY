d9618d6807536bfd9f0a37cd0c962001c7f2fae4
Tunde Yakub <tundenasri@gmail.com>
2019-05-17 10:57:56 +0100

    add versioning to route url in app js

--- server/app.js
+++ server/app.js
@@ -9,8 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/entry', entryRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Welcome to tia_diary app');
+  res.status(200).json('Welcome to tia_diary app');
 });
 
 const PORT = process.env.PORT || 3000;
@@ -18,4 +22,4 @@ app.listen(PORT, () => {
   console.log('server has started');
 });
 
-module.export = app;
+module.exports = app;

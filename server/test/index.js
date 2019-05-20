85d3428f33dde6649b7cd3d1b17153b3ac18cd26
Tunde Yakub <tundenasri@gmail.com>
2019-05-20 14:52:17 +0100

    remove invalid login route test

--- server/test/index.js
+++ server/test/index.js
@@ -6,16 +6,6 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const user = {
-  email: 'mattimobolaji@gmail.com',
-  password: 'december'
-}
-
-const user2 = {
-  email: 'mattimobolaji@gmail.com',
-  passowrd: 'dece'
-}
-
 describe('Index page test', () => {
   it('should test the index route', (done) => {
     chai
@@ -23,19 +13,6 @@ describe('Index page test', () => {
       .get('/')
       .end((err, res) => {
         expect(res.status).to.eql(200);
-        done();
-      });
-  });
-});
-
-describe('Test signup route', () => {
-  it('should signup a user', (done) => {
-    chai
-      .request(server)
-      .post('/api/v1/auth/signup')
-      .send(user)
-      .end((err, res) => {
-        expect(res.status).to.eql(201);
         done();
       });
   });
